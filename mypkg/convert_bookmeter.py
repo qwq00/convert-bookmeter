7cec1de0977b51fd3da9aad9ad6701a50b1d88ee
qwq00 <gladius.ak@gmail.com>
2022-04-18 20:39:33 +0900

    ADD import chromedriver_binary

--- mypkg/convert_bookmeter.py
+++ mypkg/convert_bookmeter.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
+import chromedriver_binary
 
 root_url = "https://bookmeter.com/"
 # Chromeを準備
